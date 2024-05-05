123a10d767efe2b89d79f79a63027070399816f9
KinnarChowdhury1994 <kc1988@cse.jgec.ac.in>
2024-05-05 17:33:04 +0530

    modified application

--- application.py
+++ application.py
@@ -1,23 +1,32 @@
-# Importing flask module in the project is mandatory
-# An object of Flask class is our WSGI application.
-from flask import Flask
- 
-# Flask constructor takes the name of 
-# current module (__name__) as argument.
-app = Flask(__name__)
- 
-# The route() function of the Flask class is a decorator, 
-# which tells the application which URL should call 
-# the associated function.
-@app.route("/")
-# ‘/’ URL is bound with hello_world() function.
-def hello_world():
-    return "<h1>Welcome to the Learning Containerization using Docker.</h1>"
+from flask import Flask 
+app = Flask(__name__) 
+@app.route('/') 
+def hello():
+    return 'Hello, Dockerized Flask App!'
 
-# main driver function
-if (__name__ == "__main__"):
+if __name__ == '__main__': 
+    app.run(host='0.0.0.0', port=5000)
+
+# # Importing flask module in the project is mandatory
+# # An object of Flask class is our WSGI application.
+# from flask import Flask
  
-    # run() method of Flask class runs the application 
-    # on the local development server.
-    # app.run(debug=True)
-    app.run()
+# # Flask constructor takes the name of 
+# # current module (__name__) as argument.
+# app = Flask(__name__)
+ 
+# # The route() function of the Flask class is a decorator, 
+# # which tells the application which URL should call 
+# # the associated function.
+# @app.route("/")
+# # ‘/’ URL is bound with hello_world() function.
+# def hello_world():
+#     return "<h1>Welcome to the Learning Containerization using Docker.</h1>"
+
+# # main driver function
+# if (__name__ == "__main__"):
+ 
+#     # run() method of Flask class runs the application 
+#     # on the local development server.
+#     # app.run(debug=True)
+#     app.run()
